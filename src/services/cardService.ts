cb29700bb946dcd26a77841a03f5993b591c76ec
Januacele <januacelevieira@hotmail.com>
2022-09-02 15:09:53 -0300

    Add logic to generate card holder name

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -3,12 +3,16 @@ import { faker } from "@faker-js/faker";
 import * as cardRepository from '../repositories/cardRepository'
 import * as employeeUtils from '../utils/employeeUtils'
 import * as cardUtils from '../utils/cardUtils'
+import * as employeeRepository from '../repositories/employeeRepository'
+
 
 export async function createCardService(companyId: number, employeeId: number, type: cardRepository.TransactionTypes){
 
     employeeUtils.checkEmployeesIsResistered(employeeId);
     employeeUtils.checkEmployeesIsFromCompany(companyId, employeeId);
     cardUtils.checkDoesNotHaveCardType(employeeId, type);
+    const numberCard: string = await generateCardNUmber('####-####-####-####');
+    const cardholderName: string = await generateCardHolderName(employeeId);
 
 
 }
@@ -25,3 +29,26 @@ async function generateCardNUmber(format: string){
     return cardNumber;
 }
 
+async function generateCardHolderName(employeeId: number){
+    const { fullName } = await employeeRepository.findById(employeeId);
+    let nameUpper = fullName.toUpperCase().split(' ');
+
+    const initialName = nameUpper[0];
+    const lastName = nameUpper[nameUpper.length - 1];
+
+    let middleName = nameUpper.filter((name, index) => {
+        return(
+            name.length >= 3 &&
+            index !== 0 &&
+            index !== nameUpper.length - 1
+        )
+    });
+
+    middleName = middleName.map(name => {
+        return name[0];
+    });
+
+    const shortMiddleName = middleName.join(' ');
+
+    return initialName + ' ' + shortMiddleName + ' ' + lastName;
+}
